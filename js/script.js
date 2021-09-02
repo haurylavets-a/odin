eeedbafb074100e42190d78e72f0f18fab58a289
Andy <Andy@mail.com>
2021-09-02 20:32:16 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log('dasd' + ' - object');
+console.log(3 + ' 4');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(--incr);
+console.log(--incr);
+console.log(incr);
+
+console.log(7%2);
+
+console.log(3*1 == `3`);
+
+console.log(3*1 === `3`);
+
+const isClosed = false,
+    isOpened = false;
+
+console.log(isClosed || !isOpened);
+
+console.log(2 + 2 * 2 != `6`);
